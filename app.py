1ffe6c7efb376edfac015203c97b6ab3eedd8a1b
Rahul Jain <16rahuljain@gmail.com>
2017-02-15 16:53:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ def index():
 
 if __name__ == "__main__":
   port = int(os.getenv('PORT', 5000))
-
-   
-
-    app.run(debug=False, port=port, host='0.0.0.0')
+  app.run(debug=False, port=port, host='0.0.0.0')
 
 
