ee7a6215e5449b4f5db427ca9de56af37ca5cc30
Rahul Jain <16rahuljain@gmail.com>
2017-03-29 15:38:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -72,14 +72,17 @@ def processRequest(req):
           "google": {
             "expect_user_response": true,
             "is_ssml": true,
-            "no_input_prompts"	[ 
+            "no_input_prompts"	
+              [ 
                 {   
                     ssml: "<audio src='http://files.jain.us.com/shrish/3.Bhajan/Ksh_Dhyan_Sagar/04meri_bhavana.mp3'>sound</audio>"
-                } 
-          ]
+                }
+              ]
           }
         }
     }
+                 
+      
        
         
     
